23b67c973d862a692d16873a880e51f978d5e116
techsinarif <techsinarif@gmail.com>
2021-12-05 18:31:29 +0530

    useState with prevState implementation

--- react-hooks/src/use_state/Counter.js
+++ react-hooks/src/use_state/Counter.js
@@ -3,10 +3,17 @@ import React, {useState} from 'react';
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const increaseFive = () => {
+    for(let i=0; i < 5; i++){
+      setCount(prevState => prevState + 1);
+    }
+  }
+
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>click me!</button>
       <p>Current value of count is {count}</p>
+      <button onClick={() => increaseFive()}>click me to add 5</button>
     </div>
   );
 };
